c9173898d77f642a7e2ad441002d647161f43781
Veronika <gavdur.veronika@gmail.ru>
2024-04-06 00:06:00 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -4,50 +4,44 @@
 // Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
 
-void Main()
-{
-    int M = ReadInt("Input number M: ");
-    int N = ReadInt("Input number N: ");
-    PrintNumbersBetweenMN(N, M);
-}
-
-void PrintNumbersBetweenMN(int numberN, int numberM)
-{
-    if (numberM > numberN || numberM == numberN)
-
-        System.Console.Write($"{numberM} >= {numberN} condition is not true");
-    else PrintAllRight(numberN, numberM);
-    PrintNumbersBetweentoMN(numberN, numberM);
-}
-
-void PrintNumbersBetweentoMN(int numberN, int numberM)
-{
-    if (numberN < numberM) return;
-    PrintNumbersBetweentoMN(numberN - 1, numberM);
-
-    System.Console.Write(numberN + ", ");
-}
-
-void PrintAllRight(int number1, int number2)
-{
-    System.Console.Write("N = " + number1 + "; M = " + number2 + " -> ");
-}
-
-int ReadInt(string msg)
-{
-    System.Console.Write(msg);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-Main();
-
-
-// using System;
-
-// class Program
+// void Main()
 // {
-//     static void Main()
-//     {
-//         Console.Write("\"Hello, World!\"");
-//     }
+//     int M = ReadInt("Input number M: ");
+//     int N = ReadInt("Input number N: ");
+//     PrintNumbersBetweenMN(N, M);
 // }
+
+// void PrintNumbersBetweenMN(int numberN, int numberM)
+// {
+//     if (numberM > numberN || numberM == numberN)
+
+//         System.Console.Write($"{numberM} >= {numberN} condition is not true");
+//     else PrintAllRight(numberN, numberM);
+//     PrintNumbersBetweentoMN(numberN, numberM);
+// }
+
+// void PrintNumbersBetweentoMN(int numberN, int numberM)
+// {
+//     if (numberN < numberM) return;
+//     PrintNumbersBetweentoMN(numberN - 1, numberM);
+
+//     System.Console.Write(numberN + ", ");
+// }
+
+// void PrintAllRight(int number1, int number2)
+// {
+//     System.Console.Write("N = " + number1 + "; M = " + number2 + " -> ");
+// }
+
+// int ReadInt(string msg)
+// {
+//     System.Console.Write(msg);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// Main();
+
+
+// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n
